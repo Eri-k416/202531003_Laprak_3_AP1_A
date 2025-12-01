128d0ac373fa2b30ee77074d9f21a3636d68ecd7
Eri_k416 <muhammadfachrydzi@gmail.com>
2025-12-01 16:49:25 +0700

    final
    think this is good

--- index.cpp
+++ index.cpp
@@ -9,21 +9,17 @@ class Tool {
         // properti objek
         int minutes;
         int price;
-
         // constructor objek, membuat objek di memori
         Tool(int mins, int p) {
             minutes = mins;
             price = p;
         };
-
         // perilaku objek, bisa menghitung harga pemakaian per menit
         long int countTotalTariff() {
             return price * minutes / 15;
         };
-
         // destructor objek, menghapus objek dalam memori
         ~Tool() = default;
-
 };
 
 // INHERITANCE
@@ -69,7 +65,7 @@ int main() {
     cout << "\t TRACKER TARIF SEWA ALAT\n";
     cout << "\t       -ARENA GYM-\n";
     cout << "=================================\n";
-    while (true) {
+    while (userChoice != 'c') {
         cout << "Pilih alat gym yang ingin dipinjam : \n";
         cout << "1. Dumbbell\n";
         cout << "2. Treadmill\n";
@@ -79,7 +75,7 @@ int main() {
         cout << "u. Undo\n";
         cout << "c. Hitung\n\n";
 
-        cout << "Ketik pilihan (hitung untuk quit): ";
+        cout << "Ketik pilihan (hitung untuk ): ";
         cin >> userChoice;
 
         switch (userChoice) {
@@ -116,17 +112,13 @@ int main() {
                 for (auto& tool : toolArray) {
                     result += tool->countTotalTariff();
                 };
-
+                toolArray.clear();
                 cout << "Total tariff yang harus dibayar : Rp." << result << endl;
                 break;
             default:
                 cout << "Pilihan tidak valid.\n\n";
                 cin.ignore(10000, '\n');
                 break;
-        };
-
-        if (userChoice == 'c') {
-            break;
         };
 
     };
